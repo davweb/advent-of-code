89f7d727dd4cf2b0191902736b40329d88a37039
David Webb <dave@davidwebb.me.uk>
2021-12-16 14:49:00 +0000

    2021 Day 15 Penultimate Tidy Up

--- advent/year2021/day15.py
+++ advent/year2021/day15.py
@@ -2,12 +2,12 @@
 
 from array import array
 from collections import deque
-from advent import taxicab_distance
+
 
 def read_input():
     rows = []
 
-    with open('input/2021/day15-input.txt') as file:
+    with open('input/2021/day15-input.txt', encoding='utf8') as file:
         for line in file.readlines():
             rows.append([int(c) for c in line.strip()])
 
@@ -18,26 +18,25 @@ def search(board):
     max_y = len(board)
     max_x = len(board[0])
 
-    bottom  = max_y - 1
+    bottom = max_y - 1
     right = max_x - 1
 
     best = []
 
-    board = array('i', (cell for row in board for cell in row ))
+    board = array('i', (cell for row in board for cell in row))
     best = array('i', (-1 for _ in range(max_x * max_y)))
-  
+
     best[0] = 0
     moves = deque((((0, 1), 0), ((1, 0), 0)))
-    
-    # Do a simple route to get an upper bound
+
+    # Do a simple route to get an initial upper bound for the risk
     best_route = sum(board[x] for x in range(max_x)) + sum(board[right + y * max_x] for y in range(1, max_y))
-    print(best_route)
 
     while moves:
         location, total_risk = moves.pop()
         x, y = location
         index = y * max_x + x
-    
+
         total_risk += board[index]
 
         if total_risk > best_route:
@@ -49,12 +48,11 @@ def search(board):
             continue
 
         best[index] = total_risk
-        
 
         if x == right and y == bottom and total_risk < best_route:
             best_route = total_risk
-            print(best_route)
 
+        # Prioritize heading for the corner
         if x < y:
 
             if x < right:
@@ -71,7 +69,7 @@ def search(board):
             if x < right:
                 moves.appendleft(((x + 1, y), total_risk))
 
-        # Going "backwards" is less likely to get to the exit so put those moves at the head of the list 
+        # Going "backwards" is less likely to get to the exit so put those moves at the head of the list
         # so they get tried later
         if x > 0:
             moves.appendleft(((x - 1, y), total_risk))
@@ -80,7 +78,6 @@ def search(board):
             moves.appendleft(((x, y - 1), total_risk))
 
     return best[bottom * max_x + right]
-
 
 
 def part1(data):
@@ -95,7 +92,7 @@ def part1(data):
 def part2(data):
     """
     >>> part2(read_input())
-    0
+    2957
     """
 
     board = []
@@ -107,14 +104,16 @@ def part2(data):
             for mx in range(5):
                 for value in old_row:
                     new_value = value + mx + my
+
                     while new_value >= 10:
                         new_value -= 9
+
                     new_row.append(new_value)
 
             board.append(new_row)
 
     return search(board)
-    
+
 
 def main():
     data = read_input()
