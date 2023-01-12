0d126507afebeda147a9df2a9099a18c98f299f5
David Webb <dave@davidwebb.me.uk>
2023-01-12 23:02:23 +0000

    2018 Day 23 Work in Progress

--- advent/year2018/day23.py
+++ advent/year2018/day23.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import re
+import math
+from collections import defaultdict
+from time import time
 
 PATTERN = re.compile(r"pos=<(-?\d+),(-?\d+),(-?\d+)>, r=(\d+)")
 
 
 def read_input():
-    file = open("input/2018/day23-input.txt", "r")
-    text = file.read()
+    with open('input/2018/day23-input.txt', encoding='utf8') as file:
+        text = file.read()
 
     nanobots = []
 
@@ -36,6 +39,21 @@ def distance(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1]) + abs(a[2] - b[2])
 
 
+def options(location, delta):
+    x, y, z = location
+    dx, dy, dz = delta
+
+    for nx in (x + dx, x - dx):
+        for ny in (y + dy, y - dy):
+            for nz in (z + dz, z - dz):
+                yield (nx, ny, nz)
+
+
+def overlapping(bot_one, bot_two):
+    journey = distance(bot_one[0], bot_two[0])
+    return journey <= bot_one[1] + bot_two[1]
+
+
 def part1(nanobots):
     """
     >>> part1([
@@ -59,9 +77,9 @@ def part1(nanobots):
     return sum(1 for (location, _) in nanobots if distance(strongest_location, location) <= strongest_range)
 
 
-def part2(nanobots):
+def part2_every_location(nanobots):
     """
-    >>> part2([
+    >>> part2_every_location([
     ...     ((10, 12, 12), 2),
     ...     ((12, 14, 12), 2),
     ...     ((16, 12, 12), 4),
@@ -70,12 +88,17 @@ def part2(nanobots):
     ...     ((10, 10, 10), 5)
     ... ])
     36
-
-    # >>> part2(read_input())
-    # 0
     """
 
-    count = {}
+    squares = 0
+
+    for bot in nanobots:
+        ((x, y, z), bot_range) = bot
+        squares += int(math.pi * 4 / 3 * bot_range)
+
+    done = 0
+    start = time()
+    count = defaultdict(int)
 
     for bot in nanobots:
         ((x, y, z), bot_range) = bot
@@ -88,21 +111,90 @@ def part2(nanobots):
 
                 for dz in range(-zrange, zrange + 1):
                     location = (x + dx, y + dy, z + dz)
+                    done += 1
 
-                    if location in count:
-                        count[location] += 1
-                    else:
-                        count[location] = 1
+                    if done % 10000000 == 0:
+                        complete = done / squares
+                        spent = time() - start
+                        total_time = spent / complete
+                        left = total_time - spent
+                        print(f'{complete * 100:.3f}% {left / 3600:.1f} hours remaining')
+
+                    count[location] += 1
+
 
     max_count = max(count.values())
     options = [location for (location, location_count) in count.items() if location_count == max_count]
     return min(distance((0, 0, 0), location) for location in options)
 
 
+def part2_by_surface(nanobots):
+    """
+    >>> part2_by_surface([
+    ...     ((10, 12, 12), 2),
+    ...     ((12, 14, 12), 2),
+    ...     ((16, 12, 12), 4),
+    ...     ((14, 14, 14), 6),
+    ...     ((50, 50, 50), 200),
+    ...     ((10, 10, 10), 5)
+    ... ])
+    36
+    """
+
+    max_count = 0
+    min_distance = None
+
+    for bot_location, bot_range in nanobots:
+        for dx in range(0, bot_range):
+            for dy in range(0, bot_range - dx):
+                delta = (dx, dy, bot_range - dx - dy)
+
+                for location in options(bot_location, delta):
+                    count = 0
+
+                    for other, other_range in nanobots:
+                        if distance(location, other) <= other_range:
+                            count += 1
+
+                    if count >= max_count:
+                        max_count = count
+                        home_distance = distance(location, (0, 0, 0))
+
+                        if min_distance is None or min_distance > home_distance:
+                            min_distance = home_distance
+
+    return home_distance
+
+
+def part2(nanobots):
+    """
+    >>> part2([
+    ...     ((10, 12, 12), 2),
+    ...     ((12, 14, 12), 2),
+    ...     ((16, 12, 12), 4),
+    ...     ((14, 14, 14), 6),
+    ...     ((50, 50, 50), 200),
+    ...     ((10, 10, 10), 5)
+    ... ])
+    6
+    >>> part2(read_input())
+    587
+    """
+
+    max_count = 0
+
+    for bot in nanobots:
+        count = sum(overlapping(bot, other) for other in nanobots)
+        max_count = max(max_count, count)
+        print(count)
+
+    return max_count
+
+
 def main():
     data = read_input()
-    print(part1(data))
-    print(part2(data))
+    #print(part1(data))
+    print(part2_every_location(data))
 
 
 if __name__ == "__main__":
