c2244c5d1364837e736f644059f1d28b11f11112
David Webb <dave@davidwebb.me.uk>
2023-01-14 15:10:37 +0000

    Linting

--- advent/year2018/day19-throwaway.py
+++ advent/year2018/day19-throwaway.py
@@ -5,8 +5,8 @@ INSTRUCTION_PATTERN = re.compile(r"([a-z]{4}) (\d+) (\d+) (\d+)")
 
 
 def read_input():
-    file = open('input/2018/day19-input.txt', 'r')
-    text = file.read()
+    with open('input/2018/day19-input.txt', encoding='utf-8') as file:
+        text = file.read()
 
     match = POINTER_PATTERN.match(text)
     register = int(match.group(1))
@@ -19,6 +19,78 @@ def read_input():
     return (register, instructions)
 
 
+def generate_commands(instructions):
+    names = ['a', 'b', 'c', 'd', 'i', 'e']
+    commands = []
+
+    for (line, (op, a, b, c)) in enumerate(instructions):
+        c = names[c]
+
+        match op:
+            case "addr":
+                a = line if names[a] == 'i' else names[a]
+                b = line if names[b] == 'i' else names[b]
+
+                if c == "i":
+                    if str(a) > str(b):
+                        (a, b) = (b, a)
+
+                    command = f"goto {a} + {b}"
+                else:
+                    command = f"{c} = {a} + {b}"
+
+            case "addi":
+                a = names[a]
+                if c == "i":
+                    b = b + line + 1
+                    command = f"goto {b}"
+                else:
+                    command = f"{c} = {a} + {b}"
+
+            case "mulr":
+                a = line if names[a] == 'i' else names[a]
+                b = line if names[b] == 'i' else names[b]
+
+                if a == "i" and b == "i" and c == "i":
+                    delta = line * line + 1
+                    command = f"goto {delta}"
+                else:
+                    command = f"{c} = {a} * {b}"
+
+            case "muli":
+                a = names[a]
+                command = f"{c} = {a} * {b}"
+
+            case "seti":
+                if c == "i":
+                    a += 1
+                    command = f"goto {a}"
+                else:
+                    command = f"{c} = {a}"
+
+            case "setr":
+                a = names[a]
+
+                if a == "i":
+                    a = line
+
+                command = f"{c} = {a}"
+
+            case "eqrr":
+                a = names[a]
+                b = names[b]
+                command = f"if {a} == {b} then {c} = 1 else {c} = 0"
+
+            case "gtrr":
+                a = names[a]
+                b = names[b]
+                command = f"if {a} > {b} then {c} = 1 else {c} = 0"
+
+        commands.append(command)
+
+    return commands
+
+
 def rewrite():
     (_, instructions) = read_input()
 
@@ -28,80 +100,7 @@ def rewrite():
         r"(.*)\n"
     )
 
-    names = ['a', 'b', 'c', 'd', 'i', 'e']
-    commands = []
-
-    for (line, (op, a, b, c)) in enumerate(instructions):
-        c = names[c]
-
-        if op == "addr":
-            a = names[a]
-
-            if a == "i":
-                a = line
-
-            b = names[b]
-
-            if b == "i":
-                b = line
-
-            if c == "i":
-                if str(a) > str(b):
-                    (a, b) = (b, a)
-
-                command = "goto {} + {}".format(a, b)
-            else:
-                command = "{} = {} + {}".format(c, a, b)
-
-        elif op == "addi":
-            a = names[a]
-            if c == "i":
-                b = b + line + 1
-                command = "goto {}".format(b)
-            else:
-                command = "{} = {} + {}".format(c, a, b)
-
-        elif op == "mulr":
-            a = names[a]
-            b = names[b]
-
-            if a == "i":
-                a = line
-
-            if b == "i":
-                b = line
-
-            if a == "i" and b == "i" and c == "i":
-                delta = line * line + 1
-                command = "goto {}".format(delta)
-            else:
-                command = "{} = {} * {}".format(c, a, b)
-        elif op == "muli":
-            a = names[a]
-            command = "{} = {} * {}".format(c, a, b)
-        elif op == "seti":
-            if c == "i":
-                a += 1
-                command = "goto {}".format(a)
-            else:
-                command = "{} = {}".format(c, a)
-        elif op == "setr":
-            a = names[a]
-
-            if a == "i":
-                a = line
-
-            command = "{} = {}".format(c, a)
-        elif op == "eqrr":
-            a = names[a]
-            b = names[b]
-            command = "if {} == {} then {} = 1 else {} = 0".format(a, b, c, c)
-        elif op == "gtrr":
-            a = names[a]
-            b = names[b]
-            command = "if {} > {} then {} = 1 else {} = 0".format(a, b, c, c)
-
-        commands.append(command)
+    commands = generate_commands(instructions)
 
     labels = {}
     words = ['apple', 'banana', 'carrot', 'doggo', 'eggs', 'fruit', 'guava']
@@ -121,17 +120,17 @@ def rewrite():
                 index += 1
                 labels[goto] = label
 
-            commands[line] = "goto {}".format(label)
+            commands[line] = f"goto {label}"
 
-        if command.startswith("goto {} + ".format(line)):
-            commands[line] = "skip {}".format(command.split()[3])
+        if command.startswith(f"goto {line} + "):
+            commands[line] = f"skip {command.split()[3]}"
 
     output = []
 
     for line, command in enumerate(commands):
         label = labels.get(line)
         label = "" if label is None else label + ":\n"
-        output.append("{}{}".format(label, command))
+        output.append(f"{label}{command}")
 
     output = "\n".join(output)
 
